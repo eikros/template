1e61f9def13b2d2e597f7a76aabd7c6f0fc8ef22
stephband <stephband@googlemail.com>
2011-09-03 02:26:16 +0200

    Globally.

--- js/site.ui.js
+++ js/site.ui.js
@@ -1,7 +1,7 @@
 jQuery(document).delegate('.js', 'click', function(e) {
 	var elem = jQuery(e.currentTarget),
 			html = elem.html(),
-			js = html.replace(/<\/?[^>]+>/gi, '').replace(/&lt;/, '<').replace(/&gt;/, '>');
+			js = html.replace(/<\/?[^>]+>/gi, '').replace(/&lt;/g, '<').replace(/&gt;/g, '>');
 	
 	eval(js);
 });
