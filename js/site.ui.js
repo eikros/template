eb7f15b0f16d1fa5c2918eb611faf87fe392c388
stephband <stephband@googlemail.com>
2011-09-03 02:24:53 +0200

    Replaces < and > in js code examples before running

--- js/site.ui.js
+++ js/site.ui.js
@@ -1,7 +1,7 @@
 jQuery(document).delegate('.js', 'click', function(e) {
 	var elem = jQuery(e.currentTarget),
 			html = elem.html(),
-			js = html.replace(/<\/?[^>]+>/gi, '');
+			js = html.replace(/<\/?[^>]+>/gi, '').replace(/&lt;/, '<').replace(/&gt;/, '>');
 	
 	eval(js);
 });
